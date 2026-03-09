5485affafbca745ad4f771b09ca22bbc2d50f2d1
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-03-09 14:58:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const contadores = document.querySelectorAll(".contador");
 
+contadores.forEach(contador => {
+
+const atualizar = () => {
+
+const valorFinal = parseFloat(contador.getAttribute("data-numero"));
+let valorAtual = parseFloat(contador.innerText);
+
+const incremento = valorFinal / 60;
+
+if(valorAtual < valorFinal){
+
+valorAtual += incremento;
+
+contador.innerText = valorAtual.toFixed(1);
+
+setTimeout(atualizar, 30);
+
+}else{
+
+contador.innerText = valorFinal.toFixed(1);
+
+}
+
+};
+
+atualizar();
+
+});
